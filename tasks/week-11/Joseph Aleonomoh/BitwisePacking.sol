867a406d3423bfaa391d75203e6446cf23d63d00
leojay-net <aleonomohjoseph03@gmail.com>
2025-03-29 16:26:02 +0100

    Bitwise Packing - Aleonomoh Joseph --Week 11

---
+++ tasks/week-11/Joseph Aleonomoh/BitwisePacking.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract BitwisePacking {
+    uint256 public packedBool;
+
+    function addValueToSlot(uint8 value, uint8 slot) public {
+        require(slot < 32, "Slot must be less than 32");
+        uint256 shiftValue = uint256(slot) * 8;
+        packedBool = (uint256(value) << shiftValue) | packedBool;
+    }
+
+    function readValueFromSlot(uint8 slot) public view returns (uint8) {
+        require(slot < 32, "Slot must be less than 32");                      
+        uint256 shiftValue = uint256(slot) * 8;
+        uint8 value = uint8((packedBool >> shiftValue) & 255);
+        return value;
+    }
+
+    function getAllValues() public view returns (uint8[] memory) {
+        uint8[] memory values = new uint8[](32);
+        for(uint8 slot=0; slot < 32; slot ++) {i
+            values[slot] = readValueFromSlot(slot);
+        }
+        return values;
+    }
+}
